1cc49c1e0f42b9fc560318a126d999013be69610
SUSocean <igonind.a1@gmail.com>
2023-05-07 16:42:01 +0300

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+export const Header = () => {
+    return (
+        <nav className="w-full h-10 bg-black">
+            <ul className="max-w-4xl ml-auto px-10 flex justify-end gap-6 text-white">
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Header
