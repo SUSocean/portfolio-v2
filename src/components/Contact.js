392070a17f204aaf1e7d6bbf9bcd393595cc9a8f
SUSocean <igonind.a1@gmail.com>
2023-05-12 13:51:31 +0300

    buttons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ const Contact = () => {
     return (
         <section id="contact" className="mx-auto w-5/6 min-h-screen max-w-5xl">
             <div className='flex place-items-center gap-5 mb-20'>
-                <h3 className="text-slate-200 text-2xl font-semibold sm:4xl">Get In Touch</h3>
+                <h3 className="text-slate-200 text-2xl font-semibold sm:text-4xl">Get In Touch</h3>
                 <span className='w-72 h-0.5 bg-slate-600 sm:w-80'></span>
             </div>
             <div className="min-h-[calc(100vh-116px)] flex flex-col justify-center">
@@ -11,7 +11,7 @@ const Contact = () => {
                     questions or just want to say Hi, my inbox is always open.
                 </article>
 
-                <button className='border-2 border-cyan-300 px-12 block mx-auto py-2 rounded-md text-cyan-300 cursor-pointer relative hover:-translate-x-1 hover:-translate-y-1 hover:border-r-4 hover:border-b-4 transition-all duration-200'>Say Hi</button>
+                <a className='border-2 border-cyan-300 px-12 block mx-auto py-2 rounded-md text-cyan-300 cursor-pointer relative hover:-translate-x-1 hover:-translate-y-1 hover:border-r-4 hover:border-b-4 transition-all duration-200'>Say Hi</a>
             </div>
         </section>
     )
